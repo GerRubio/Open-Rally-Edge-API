e6ebf3448dc44dd6a386dc884433d30161237206
[GerRubio] <[rubio.delavieja@gmail.com]>
2024-04-23 16:34:56 +0200

    Voters for Users and Project implemented

--- api/src/Security/Authorization/Voter/ProjectVoter.php
+++ api/src/Security/Authorization/Voter/ProjectVoter.php
@@ -2,32 +2,60 @@
 
 namespace App\Security\Authorization\Voter;
 
+use App\Entity\Project;
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class ProjectVoter extends Voter
 {
+    public const PROJECT_CREATE = 'PROJECT_CREATE';
     public const PROJECT_READ = 'PROJECT_READ';
+    public const PROJECT_UPDATE = 'PROJECT_UPDATE';
     public const PROJECT_DELETE = 'PROJECT_DELETE';
+
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
 
     protected function supports(string $attribute, mixed $subject): bool
     {
-        return \in_array($attribute, $this->supportedAttributes(), true);
+        return in_array($attribute, $this->supportedAttributes(), true) && $subject instanceof Project;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
     {
-        if (\in_array($attribute, $this->supportedAttributes(), true)) {
+        if (!$subject instanceof Project) {
+            return false;
+        }
+
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
             return true;
         }
 
-        return false;
+        return match ($attribute) {
+            self::PROJECT_CREATE => true,
+            self::PROJECT_READ, self::PROJECT_UPDATE, self::PROJECT_DELETE => $subject->getOwner()->getId() === $user->getId() || $this->security->isGranted('ROLE_ADMIN'),
+            default => false,
+        };
     }
 
     private function supportedAttributes(): array
     {
         return [
+            self::PROJECT_CREATE,
             self::PROJECT_READ,
+            self::PROJECT_UPDATE,
             self::PROJECT_DELETE,
         ];
     }
--- api/src/Security/Authorization/Voter/UserVoter.php
+++ api/src/Security/Authorization/Voter/UserVoter.php
@@ -2,32 +2,56 @@
 
 namespace App\Security\Authorization\Voter;
 
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class UserVoter extends Voter
 {
     public const USER_READ = 'USER_READ';
+    public const USER_UPDATE = 'USER_UPDATE';
     public const USER_DELETE = 'USER_DELETE';
+
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
 
     protected function supports(string $attribute, mixed $subject): bool
     {
-        return \in_array($attribute, $this->supportedAttributes(), true);
+        return in_array($attribute, $this->supportedAttributes(), true) && $subject instanceof User;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
     {
-        if (\in_array($attribute, $this->supportedAttributes(), true)) {
+        if (!$subject instanceof User) {
+            return false;
+        }
+
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
             return true;
         }
 
-        return false;
+        return match ($attribute) {
+            self::USER_READ, self::USER_UPDATE, self::USER_DELETE => $subject->getId() === $user->getId(),
+            default => false,
+        };
     }
 
     private function supportedAttributes(): array
     {
         return [
             self::USER_READ,
+            self::USER_UPDATE,
             self::USER_DELETE,
         ];
     }
